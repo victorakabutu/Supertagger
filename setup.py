5c94bb4291b25c8affc5bccd6e015f6213326aca
Victor Akabutu <iversion@gmail.com>
2013-09-26 12:42:07 -0400

    Cleaned up code in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 from setuptools import setup
 
-setup( name = 'MP3 Supertagger',
-       version = '0.3',
-       description = 'MP3 Supertagger properly tags all MP3s in a specified folder',
-       author = 'Victor Akabutu',
-       author_email = 'iversion@gmail.com',
-       install_requires = ['mutagen>=1.22',
-                           'musicbrainzngs >=0.4'
-                           ]
-       packages = find_packages()
+setup(name = 'MP3 Supertagger',
+	version = '0.3',
+	description = 'MP3 Supertagger properly tags all MP3s in a specified folder',
+	author = 'Victor Akabutu',
+	author_email = 'iversion@gmail.com',
+	install_requires = ['mutagen>=1.22', 'musicbrainzngs >=0.4']
+	packages = find_packages()
 )
